b317ae235b590d16f4eed0ec3cd64eb7506f39f3
Joplay42 <jojobanjo2611@gmail.com>
2024-02-01 22:34:58 -0500

    v0.1

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -14,7 +14,7 @@ const SideBar = ({ index, setIndex }: Props) => {
     return (
         <div id="sidebar" className="hidden xl:block bg-white py-20 lg:py-10 px-4 2xl:px-10 ml-16 mt-6 mr-6 rounded-3xl lg:w-2/5 2xl:w-2/6 relative order-1 shadow-xl border border-gray-200 dark:border-neutral-800 dark:bg-neutral-900">
             <div className="absolute top-1/2 transform -translate-y-1/2 lg:top-auto lg:transform-none lg:-translate-y-0 lg:static py-4 lg:py-0 flex flex-wrap lg:flex-col">
-                {nav.map(nav => (
+                {nav.map(nav => ( 
                     <ListItem 
                         index={index} 
                         name={nav.name} 
