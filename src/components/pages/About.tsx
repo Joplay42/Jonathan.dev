2ade06d2879e0875cd674daa6d03618fbc434872
Joplay42 <jojobanjo2611@gmail.com>
2024-02-03 20:23:13 -0500

    v0.1.6

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,14 +1,53 @@
 const About = () => {
     return (
         <div className=" bg-white dark:bg-neutral-900 dark:text-white rounded-3xl shadow-xl border border-gray-200 dark:border-neutral-800 overflow-y-auto px-4 md:px-14 lg:px-20 xl:px-24 2xl:px-32 mt-6 mr-6 xl:mr-16 ml-6 mb-6">
-            <div className="h-[70vh] ">
-                <section className="grid lg:grid-cols-3 justify-between items-center gap-5 2xl:gap-10 py-4 md:py-14">
-                        <div className="order-2 lg:order-1 max-w-5xl col-span-2">
-                            <h4 className="font-bold text-blue-700 dark:text-white mb-3"><span className="text-gray-400 text-xl mr-2 dark:text-sky-700">01</span> À propos de moi</h4><hr className="border-neutral-400 dark:border-neutral-700" />
-                            <p className="mt-6 text-gray-500 dark:text-neutral-500">Hey, je m'appelle Jonathan Deschênes et je suis à la recherche d'un emploi en tant que designer et programmeur web. J'ai de l'expérience dans ce domaine, autrement dit j'ai réaliser plusieurs sites professionel pour d'autre compagnie.</p><br />
-                            <p className="text-gray-500 dark:text-neutral-500"><span className="font-semibold text-sky-700">Pour en expliquer un peu plus sur moi et comment j'ai choisit le domaine du web :</span><br /> J'ai toujours eu une passion pour les jeux vidéo, donc biensûr comme chaque jeune j'ai voulût être développeur de jeux vidéo. Ensuite, dans mon étude en informatique j'ai découvert la programmation, celle-ci m'a fait découvrir l'existence du web et des milliers de possibilité de créations. Lors de mon stage de designer web, j'ai développé mes connaissances en web. À ce moment j'ai découvert ma passion pour le domaine du web.</p>
+            <div className="h-[70vh]">
+                <section className="grid grid-cols-3 justify-between items-center gap-5 2xl:gap-10 py-10 lg:py-0">
+                        <div className="order-1 max-w-5xl col-span-3 md:col-span-2">
+                            <h4 className="font-bold text-blue-700 dark:text-white mb-3 text-xl"><span className="text-gray-400 text-2xl mr-2 dark:text-sky-700">01</span> À propos de moi</h4><hr className="border-neutral-400 dark:border-neutral-700" />
+                            <p className="mt-6 text-gray-600 font-medium dark:text-neutral-500 leading-6">Hey, je m'appelle Jonathan Deschênes et je suis à la recherche d'un emploi en tant que designer et programmeur web. J'ai de l'expérience dans ce domaine, autrement dit j'ai réaliser plusieurs sites professionel pour d'autre compagnie.</p><br />
+                            <p className="text-gray-600 font-medium dark:text-neutral-500 leading-6"><span className="font-semibold text-sky-700">Pour en expliquer un peu plus sur moi et comment j'ai choisit le domaine du web :</span><br /> J'ai toujours eu une passion pour les jeux vidéo, donc biensûr comme chaque jeune j'ai voulût être développeur de jeux vidéo. Ensuite, dans mon étude en informatique j'ai découvert la programmation, celle-ci m'a fait découvrir l'existence du web et des milliers de possibilité de créations. Lors de mon stage de designer web, j'ai développé mes connaissances en web. À ce moment j'ai découvert ma passion pour le domaine du web.</p>
                         </div>
-                        <img src="/support-center.png" className="w-80 lg:w-[28rem] justify-self-center col-span-2 lg:col-span-1 h-auto order-1 lg:order-2" />
+                        <img src="/support-center.png" className="hidden md:block w-40 md:w-[28rem] justify-self-center col-span-1 h-auto order-2" />
+                </section>
+                <section className="py-20 xl:py-0">
+                    <h2 className="text-center text-xl text-neutral-600 dark:text-neutral-300">Mon parcours scolaire</h2>
+                    <div className="grid grid-col-1 lg:grid-cols-2 lg:grid-rows-5 gap-x-24 relative mt-10 lg:mt-0">
+                        <div className="relative lg:row-start-2">
+                            <div className="border border-neutral-300 dark:border-neutral-600 rounded-lg p-4">
+                                <div className="bg-blue-700 dark:bg-sky-700 text-white text-xs font-bold px-2 py-1 rounded-full w-fit mb-3">
+                                    <p>2017-2022</p>
+                                </div>
+                                <h4 className="font-semibold text-blue-700 dark:text-sky-700">Diplôme étude secondaire - DES</h4>
+                                <p className="font-light leading-10">Centre professionel des riverains</p>
+                            </div>
+                            <div className="hidden lg:block z-50 bg-blue-700 text-blue-700 px-1 leading-3 rounded-full ring-1 ring-neutral-500 ring-offset-4 absolute -right-[54px] top-14">.</div>
+                        </div>
+                        <div className="relative lg:row-start-3 lg:col-start-2">
+                            <div className="border border-neutral-300 dark:border-neutral-600 rounded-lg p-4">
+                                <div className="bg-blue-700 dark:bg-sky-700 text-white text-xs font-bold px-2 py-1 rounded-full w-fit mb-3">
+                                    <p>2020-2023</p>
+                                </div>
+                                <h4 className="font-semibold text-blue-700 dark:text-sky-700">Diplôme etude professionel - DEP</h4>
+                                <p className="font-semibold leading-10">Soutien informatique</p>
+                                <p className="font-light">Centre professionel des riverains</p>
+                            </div>
+                            <div className="hidden lg:block z-50 bg-blue-700 text-blue-700 px-1 leading-3 rounded-full ring-1 ring-neutral-500 ring-offset-4 absolute -left-[54px] top-14">.</div>
+                        </div>
+                        <div className="relative lg:row-start-4">
+                            <div className="border border-neutral-300 dark:border-neutral-600 rounded-lg p-4">
+                                <div className="bg-blue-700 dark:bg-sky-700 text-white text-xs font-bold px-2 py-1 rounded-full w-fit mb-3">
+                                    <p>En cours</p>
+                                </div>
+                                <h4 className="font-semibold text-blue-700 dark:text-sky-700">Diplôme étude collégiale - DEC</h4>
+
+                                <p className="font-semibold leading-10">Technique en programmation</p>
+                                <p className="font-light">Cégep Montmorency</p>
+                            </div>
+                            <div className="hidden lg:block z-50 bg-blue-700 text-blue-700 px-1 leading-3 rounded-full ring-1 ring-neutral-500 ring-offset-4 absolute -right-[54px] top-14">.</div>
+                        </div>
+                        <div className="hidden lg:block absolute inset-y-0 left-1/2 top-1/2 bg-neutral-300 dark:bg-neutral-600 w-[1px] transform -translate-y-1/2 -translate-x-1/2 h-[35rem]"></div>                    
+                    </div>
                 </section>
             </div>
         </div>
--- src/components/pages/Accueil.tsx
+++ src/components/pages/Accueil.tsx
@@ -5,12 +5,12 @@ interface Props {
 const Accueil = ({ setIndex }: Props) => {
     return (
         <div className=" bg-white dark:bg-neutral-900 dark:text-white rounded-3xl shadow-xl border border-gray-200 dark:border-neutral-800 overflow-y-auto px-4 md:px-14 lg:px-20 xl:px-24 2xl:px-32 mt-6 mr-6 xl:mr-16 ml-6 mb-6">
-            <div className="h-[70vh] ">
+            <div className="h-[70vh]">
                 <section className="grid lg:grid-cols-2 justify-between items-center gap-5 2xl:gap-10 py-4 md:py-14">
                     <div className="order-2 lg:order-1 max-w-5xl">
                         <h4 className="font-bold dark:text-neutral-400 mb-3">Hey, <span className="font-medium">je me présente</span></h4>
-                        <h1 className=" text-3xl 2xl:text-4xl font-semibold leading-9"><span className="text-sky-700">Jonathan</span> Deschênes</h1>
-                        <h1 className="text-neutral-600 dark:text-neutral-600 text-3xl 2xl:text-4xl font-bold leading-10">Je suis developper Web</h1>
+                        <h1 className=" text-2xl font-bold leading-3 mt-8"><span className="text-sky-700">Jonathan</span> Deschênes</h1>
+                        <h1 className="text-neutral-600 dark:text-neutral-600 text-2xl font-bold leading-5 mb-7">Je suis developper Web</h1>
                         <p className="leading-6 mt-3">Je suis un passioné de l'informatique. En d'autre mot un expert en développement web, avec beaucoup d'expériences dans le domaine je sais comment me surpasser.</p>
                         <button className="bg-blue-700 hover:bg-transparent hover:text-blue-700 border border-blue-700 dark:bg-blue-700 dark:border-blue-700 dark:hover:text-white hover:dark:bg-transparent text-white px-6 py-3 rounded-lg mt-4" onClick={() => {setIndex("Contact")}}>Me contacter</button>
                     </div>
@@ -19,7 +19,7 @@ const Accueil = ({ setIndex }: Props) => {
                 <section className="grid lg:grid-cols-2 justify-between items-center gap-5 2xl:gap-10 py-4 md:py-14">
                     <div className="order-2 lg:order-2 max-w-5xl">
                         <h4 className="font-bold text-sky-700 mb-3">À propos</h4>
-                        <h1 className=" dark:text-white text-3xl 2xl:text-4xl font-semibold leading-10">Qui <span className="text-sky-700">suis</span>-je?</h1>
+                        <h1 className=" dark:text-white text-2xl font-semibold leading-10">Qui <span className="text-sky-700">suis</span>-je?</h1>
                         <p className="leading-6 mt-3">Un jeune passioné qui recherche un emploi en tant que designer/programmeur web. Cliquez en dessu pour en apprendre plus sur moi!</p>
                         <button className="bg-blue-700 hover:bg-transparent hover:text-blue-700 border border-blue-700 dark:bg-blue-700 dark:border-blue-700 dark:hover:text-white hover:dark:bg-transparent text-white px-6 py-3 rounded-lg mt-4" onClick={() => {setIndex("About")}}>À propos de moi</button>
                     </div>
@@ -28,7 +28,7 @@ const Accueil = ({ setIndex }: Props) => {
                 <section className="grid lg:grid-cols-2 justify-between items-center gap-5 2xl:gap-10 py-4 md:py-14">
                     <div className="order-2 lg:order-1 max-w-5xl">
                         <h4 className="font-bold text-sky-700 mb-3">Expériences</h4>
-                        <h1 className=" dark:text-white text-3xl 2xl:text-4xl font-semibold leading-10">Mon parcours</h1>
+                        <h1 className=" dark:text-white text-2xl font-semibold leading-10">Mon parcours</h1>
                         <p className="leading-6 mt-3">Un futur programmeur qui étudie en temp plein pour expandre mes compétences.</p>
                         <button className="bg-blue-700 hover:bg-transparent hover:text-blue-700 border border-blue-700 dark:bg-blue-700 dark:border-blue-700 dark:hover:text-white hover:dark:bg-transparent text-white px-6 py-3 rounded-lg mt-4" onClick={() => {setIndex("Experiences")}}>Mes expériences</button>
                     </div>
@@ -37,7 +37,7 @@ const Accueil = ({ setIndex }: Props) => {
                 <section className="grid lg:grid-cols-2 justify-between items-center gap-5 2xl:gap-10 py-4 md:py-14">
                     <div className="order-2 lg:order-2 max-w-5xl">
                         <h4 className="font-bold text-sky-700 mb-3">Réalisation</h4>
-                        <h1 className=" dark:text-white text-3xl 2xl:text-4xl font-semibold leading-10">Réalisation de <span className="text-sky-700">plusieurs site</span> pour des compagnies</h1>
+                        <h1 className=" dark:text-white text-2xl font-semibold leading-10">Réalisation de <span className="text-sky-700">plusieurs site</span> pour des compagnies</h1>
                         <p className="leading-6 mt-3">Durant mon parcours j'ai eu la chance de réaliser des sites pour des compagnies et des organismes, tous étant des magnifiques design.</p>
                         <button className="bg-blue-700 hover:bg-transparent hover:text-blue-700 border border-blue-700 dark:bg-blue-700 dark:border-blue-700 dark:hover:text-white hover:dark:bg-transparent text-white px-6 py-3 rounded-lg mt-4" onClick={() => {setIndex("Realisation")}}>Mes réalisations</button>
                     </div>
