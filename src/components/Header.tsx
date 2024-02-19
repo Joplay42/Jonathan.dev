2731fdce3185b2f9e57253d7f60e3999ad4a11b1
Joplay42 <jojobanjo2611@gmail.com>
2024-02-19 09:25:42 -0500

    v1.0.0

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,44 +1,57 @@
 import { FaGithub } from "react-icons/fa";
 import { IoLogoLinkedin } from "react-icons/io";
 import { IoMenuOutline } from "react-icons/io5";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { useEffect } from 'react';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const Header = () => {
-    function handleMobileMenu() {
-        const sidebar = document.getElementById("sidebar");
+  function handleMobileMenu() {
+    const sidebar = document.getElementById("sidebar");
 
-        if (sidebar) {
-            const isHidden = sidebar.classList.contains("hidden");
-            sidebar.classList.toggle("hidden");
+    if (sidebar) {
+      const isHidden = sidebar.classList.contains("hidden");
+      sidebar.classList.toggle("hidden");
 
-            sidebar.style.marginLeft = isHidden ? "16px" : "64px";
-        }
+      sidebar.style.marginLeft = isHidden ? "16px" : "64px";
     }
+  }
 
-    useEffect(() => {
-        AOS.init();
-    }, [])
+  useEffect(() => {
+    AOS.init();
+  }, []);
 
-    return (
-        <header data-aos="fade-down" data-aos-duration="1000" className="bg-white dark:bg-neutral-900 p-6 lg:p-14 mx-4 xl:mx-16 mt-4 lg:mt-10 rounded-3xl shadow-xl border border-gray-200 dark:border-neutral-800">
-            <div className="flex justify-between items-center">
-                <div className="flex justify-between items-center gap-6">
-                    <IoMenuOutline onClick={handleMobileMenu}  className="xl:hidden w-8 h-8 dark:text-white"/>
-                    <h1 className="font-semibold text-3xl dark:text-white">Jonathan<span className="text-blue-700 text-2xl font-bold">.Dev</span></h1>
-                </div>
-                <div className="hidden md:flex justify-between items-center gap-6 dark:text-white">
-                    <a href="https://github.com/Joplay42" target="_blank">
-                        <FaGithub className="w-8 h-8"/>
-                    </a>
-                    <a href="https://www.linkedin.com/" target="_blank">
-                        <IoLogoLinkedin className="w-10 h-10"/>
-                    </a>
-                </div>
-            </div>
-        </header>
-    )
-}
+  return (
+    <header
+      data-aos="fade-down"
+      data-aos-duration="1000"
+      className="bg-white dark:bg-neutral-900 p-6 lg:p-14 mx-4 xl:mx-16 mt-4 lg:mt-10 rounded-3xl shadow-xl border border-gray-200 dark:border-neutral-800"
+    >
+      <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center gap-6">
+          <IoMenuOutline
+            onClick={handleMobileMenu}
+            className="xl:hidden w-8 h-8 dark:text-white"
+          />
+          <h1 className="font-semibold text-3xl dark:text-white">
+            Jonathan
+            <span className="text-blue-700 text-2xl font-bold">.Dev</span>
+          </h1>
+        </div>
+        <div className="hidden md:flex justify-between items-center gap-6 dark:text-white">
+          <a href="https://github.com/Joplay42" target="_blank">
+            <FaGithub className="w-8 h-8" />
+          </a>
+          <a
+            href="www.linkedin.com/in/jonathan-deschênes-4276802b5"
+            target="_blank"
+          >
+            <IoLogoLinkedin className="w-10 h-10" />
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
